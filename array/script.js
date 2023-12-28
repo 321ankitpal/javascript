6243f0c844fcd20f4e42118f73a5ec91eec7981e
first-demo <321ankitpal@gmail.com>
2023-12-28 15:34:47 +0530

    modern javascript

--- array/script.js
+++ array/script.js
@@ -46,3 +46,59 @@ for(let i=0;i<item.length;i++){
   item[i]=item[i]-offer
 }
 console.log(item)
+
+// array method => function of array
+
+// push()=> add to end ,changes original array
+// pop()=>delete from end and return, changes original array
+// toString()=>convert into string, changes a new array 
+
+let food=['apple','banana','grapes','lichi']
+
+food.push("chips","stoberry")
+
+console.log(food)
+
+let deleted=food.pop();
+console.log(deleted)
+
+console.log(food.toString());
+
+// .concat()=> join multiple array and return result, changes on  create new array
+
+preach=["ankit","aayush","aditya","rohit"]
+quality=["speaker","quality","writer","louder"]
+
+ console.log(preach.concat(quality))
+
+//  unshift() => add the element on the strating,changes on original array
+// shift() => delete the element on the starting,changes on original array
+
+// slice()=>return a piece of the array, change on new create array
+// slice(startidx,endidx)
+
+console.log(preach.slice(1,3)); //last number is exclusiive
+
+// splice()=> changes original array (add,remove,replace)
+//   splice(startidx,delidx,new)
+
+let arr1=[1,2,3,4,5,6,7,8]
+
+arr1.splice(2,2,101,102)
+console.log(arr1);
+
+// arr1.splice(3);=> it is deleted element of after the index 3, work as slice
+
+
+// practice q remove first ele ,add one thing ,addd amazon at end
+
+company=["bloomberg","microsoft","uber","google","ibm","netflix"]
+
+console.log(company.shift());
+console.log(company);
+
+company.splice(2,1,"ola");
+console.log(company)
+
+company.push("amazon")
+console.log(company);
---
+++ function and method/script.js
@@ -0,0 +1,90 @@
+// functions and method
+
+// block of code that perform a specific task , can be invoked whenever needed
+
+function myfunction(msg){ //=> msg it is type of input called as parameter
+ console.log(msg)
+}
+myfunction(" i love jesus")// argument
+
+function sum(a,b){ // a,b are local variable of function ,  it mean block scope  
+    s=a+b
+    return s;
+}
+
+let val=sum(3,4);
+console.log(val)
+
+// return ke baad function me aap koi bhi code likhe vo excute nahi hota hai
+
+// arrow function is compact written type of a function
+
+const mul=  (a,b)=>{
+ console.log(a*b);
+}
+
+// practice q take string find vowels on it
+
+// function countvow(str){
+//     let count=0
+//     for(let char of str){
+//         if(char==="a"|| char==="e"||char==="i"|| char==="o"||char ==="u"){
+//             count=count+1;
+          
+//         }
+//     }
+//     return count;
+// }
+// let vowels=countvow("jesusiloveu")
+// console.log(vowels)
+
+let vowels=(str)=>{
+    let count=0;
+  for(let char of str){
+    if(char==="a"|| char==="e"||char==="i"|| char==="o"||char ==="u"){
+  count++;
+
+    }
+  }
+  console.log(count);
+}
+ vowels("apnacollege");
+
+
+// for each loop in array => method which is used to only array not on string
+// arr.forEach(callbackFunction)
+// callbackFunction : here ,it is a function to excute for each element in the array
+// a callback is function passsed as an argument to another function
+
+let arr=[1,2,3,4,5,6]
+//  arr.forEach(function myfunc(val){
+// console.log(val)
+//  })
+
+ arr.forEach((val,idx,arr)=>{
+console.log(val,idx,arr);
+ })
+
+//  heighr order function is type of function which take other function as parameter and also return function
+
+// practice q find sqaure of each array value
+
+// let square=[1,2,3,4,5,6,7]
+
+// square.forEach((val)=>{
+//   console.log(val**2)
+   
+// }
+
+// )
+
+function square(x){
+  console.log( x**2);
+
+
+}
+let x=[1,2,3,4,5,6,7]
+ 
+
+x.forEach( square)
+   
